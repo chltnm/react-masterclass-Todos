e6cc40a5dbd572807eefea3004b257f934391783
chltnm <soomin925@gmail.com>
2021-12-16 04:51:52 +0900

    local storage

--- src/components/CreateToDo.tsx
+++ src/components/CreateToDo.tsx
@@ -4,23 +4,34 @@ import { categoryState, toDoState } from "../atoms";
 
 interface IForm {
   toDo: string; // input에서 submit한 value 이름
+  category: string;
 }
 
 function CreateToDo() {
   const setToDos = useSetRecoilState(toDoState);
   const category = useRecoilValue(categoryState);
-  const { register, handleSubmit, setValue } = useForm();
+  const { register, handleSubmit, setValue } = useForm<IForm>();
 
-  const handleValid = ({ toDo }: IForm) => {
+  const handleValid = (data: IForm) => {
     setToDos((oldToDos) => [
-      { text: toDo, id: Date.now(), category }, //설정되어있는 category 에서 list 생성
+      { text: data.toDo, id: Date.now(), category }, //설정되어있는 category 에서 list 생성
       ...oldToDos,
     ]);
     setValue("toDo", "");
+    setValue("category", "");
   };
 
   return (
     <form onSubmit={handleSubmit(handleValid)}>
+      {/* <input
+        {...register("category", {
+          required: false,
+        })}
+        placeholder="create category"
+      ></input>
+      <button>Add Category</button>
+      <br></br>
+      <br></br> */}
       <input
         {...register("toDo", {
           required: "Please write a To Do",
